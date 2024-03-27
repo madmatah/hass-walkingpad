f511f6324f7ce38f39f33262109d25d3661de3a2
Matthieu Huguet <matthieu@huguet.eu>
2024-03-27 21:32:36 +0000

    ✨ import prototype of the king_smith custom integration
    A prototype of the walkingpad custom integration was created with the following features:
    - support for WalkingPad A1 Pro
    - manual config flow
    - bluetooth discovery config flow
    - distance sensor
    - steps sensor
    - duration sensors (available in several units : minutes, hours, days)
    - current speed sensor
    
    This commit integrates the prototype into the project.

--- custom_components/king_smith/sensor.py
+++ custom_components/king_smith/sensor.py
@@ -1,46 +1,147 @@
-"""Sensor platform for king_smith."""
+"""WalkingPad sensor support."""
 from __future__ import annotations
 
-from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
+from collections.abc import Callable
+from dataclasses import dataclass
 
-from .const import DOMAIN
-from .coordinator import BlueprintDataUpdateCoordinator
-from .entity import IntegrationBlueprintEntity
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorEntityDescription,
+    SensorStateClass,
+)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import UnitOfLength, UnitOfSpeed, UnitOfTime
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import StateType
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-ENTITY_DESCRIPTIONS = (
-    SensorEntityDescription(
-        key="king_smith",
-        name="Kingsmith WalkingPad",
-        icon="mdi:format-quote-close",
+from . import WalkingPadIntegrationData
+from .const import DOMAIN, WalkingPadStatus
+from .coordinator import WalkingPadCoordinator
+
+
+@dataclass(kw_only=True)
+class WalkingPadSensorEntityDescription(SensorEntityDescription):
+    """Describes Example sensor entity."""
+
+    value_fn: Callable[[WalkingPadStatus], StateType]
+
+
+SENSORS: tuple[WalkingPadSensorEntityDescription, ...] = (
+    WalkingPadSensorEntityDescription(
+        device_class=SensorDeviceClass.DISTANCE,
+        icon="mdi:walk",
+        key="walkingpad_distance",
+        name=None,
+        native_unit_of_measurement=UnitOfLength.KILOMETERS,
+        state_class=SensorStateClass.TOTAL_INCREASING,
+        suggested_display_precision=2,
+        suggested_unit_of_measurement=UnitOfLength.KILOMETERS,
+        translation_key="walkingpad_distance",
+        value_fn=lambda status: status.get("session_distance", 0.0) / 1000,
+    ),
+    WalkingPadSensorEntityDescription(
+        icon="mdi:shoe-print",
+        key="walkingpad_steps",
+        name=None,
+        native_unit_of_measurement="steps",
+        state_class=SensorStateClass.TOTAL_INCREASING,
+        suggested_display_precision=0,
+        translation_key="walkingpad_steps",
+        value_fn=lambda status: status.get("session_steps", 0),
+    ),
+    WalkingPadSensorEntityDescription(
+        icon="mdi:timer",
+        key="walkingpad_duration_minutes",
+        name=None,
+        native_unit_of_measurement=UnitOfTime.MINUTES,
+        state_class=SensorStateClass.TOTAL_INCREASING,
+        suggested_display_precision=0,
+        suggested_unit_of_measurement=UnitOfTime.MINUTES,
+        translation_key="walkingpad_duration_minutes",
+        value_fn=lambda status: round(status.get("session_running_time", 0) / 60, 1),
+    ),
+    WalkingPadSensorEntityDescription(
+        icon="mdi:timer",
+        key="walkingpad_duration_hours",
+        name=None,
+        native_unit_of_measurement=UnitOfTime.HOURS,
+        state_class=SensorStateClass.TOTAL_INCREASING,
+        suggested_display_precision=1,
+        suggested_unit_of_measurement=UnitOfTime.HOURS,
+        translation_key="walkingpad_duration_hours",
+        value_fn=lambda status: round(status.get("session_running_time", 0) / 3600, 4),
+    ),
+    WalkingPadSensorEntityDescription(
+        icon="mdi:timer",
+        key="walkingpad_duration_days",
+        name=None,
+        native_unit_of_measurement=UnitOfTime.DAYS,
+        state_class=SensorStateClass.TOTAL_INCREASING,
+        suggested_display_precision=1,
+        suggested_unit_of_measurement=UnitOfTime.DAYS,
+        translation_key="walkingpad_duration_days",
+        value_fn=lambda status: round(status.get("session_running_time", 0) / 86400, 6),
+    ),
+    WalkingPadSensorEntityDescription(
+        device_class=SensorDeviceClass.SPEED,
+        icon="mdi:speedometer",
+        key="walkingpad_current_speed",
+        name=None,
+        native_unit_of_measurement=UnitOfSpeed.KILOMETERS_PER_HOUR,
+        state_class=SensorStateClass.MEASUREMENT,
+        suggested_display_precision=1,
+        suggested_unit_of_measurement=UnitOfSpeed.KILOMETERS_PER_HOUR,
+        translation_key="walkingpad_current_speed",
+        value_fn=lambda status: status.get("speed", 0.0),
     ),
 )
 
 
-async def async_setup_entry(hass, entry, async_add_devices):
-    """Set up the sensor platform."""
-    coordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_devices(
-        IntegrationBlueprintSensor(
-            coordinator=coordinator,
-            entity_description=entity_description,
-        )
-        for entity_description in ENTITY_DESCRIPTIONS
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+) -> None:
+    """Set up the WalkingPad sensors."""
+
+    entry_data: WalkingPadIntegrationData = hass.data[DOMAIN][entry.entry_id]
+    coordinator = entry_data["coordinator"]
+
+    async_add_entities(
+        WalkingPadSensor(coordinator, description) for description in SENSORS
     )
 
 
-class IntegrationBlueprintSensor(IntegrationBlueprintEntity, SensorEntity):
-    """king_smith Sensor class."""
+class WalkingPadSensor(
+    CoordinatorEntity[WalkingPadCoordinator],
+    SensorEntity,
+):
+    """Represent a WalkingPad sensor."""
+
+    entity_description: WalkingPadSensorEntityDescription
+
+    _attr_has_entity_name = True
 
     def __init__(
         self,
-        coordinator: BlueprintDataUpdateCoordinator,
-        entity_description: SensorEntityDescription,
+        coordinator: WalkingPadCoordinator,
+        entity_description: WalkingPadSensorEntityDescription,
     ) -> None:
-        """Initialize the sensor class."""
+        """Initialize the sensor."""
         super().__init__(coordinator)
+
         self.entity_description = entity_description
+        self._attr_unique_id = (
+            f"{coordinator.walkingpad_device.mac}-{self.entity_description.key}"
+        )
 
     @property
-    def native_value(self) -> str:
-        """Return the native value of the sensor."""
-        return self.coordinator.data.get("body")
+    def native_value(self) -> StateType:
+        """Return the state of the sensor."""
+        return self.entity_description.value_fn(self.coordinator.data)
+
+    @property
+    def available(self) -> bool:
+        """Return if entity is available."""
+        return self.coordinator.connected
---
+++ custom_components/king_smith/walkingpad.py
@@ -0,0 +1,134 @@
+"""Walking Pad Api."""
+import asyncio
+from enum import Enum, unique
+import logging
+
+from bleak import BleakError
+from bleak.backends.device import BLEDevice
+from ph4_walkingpad.pad import Controller, WalkingPadCurStatus
+
+from .const import BeltState, WalkingPadMode, WalkingPadStatus
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@unique
+class WalkingPadConnectionStatus(Enum):
+    """An enumeration of the possible connection states."""
+
+    NOT_CONNECTED = 0
+    CONNECTING = 1
+    CONNECTED = 2
+
+
+class WalkingPad:
+    """The WalkingPad device."""
+
+    def __init__(self, name: str, ble_device: BLEDevice) -> None:
+        """Create a WalkingPad object."""
+
+        self._name = name
+        self._ble_device = ble_device
+        self._controller = Controller()
+        self._controller.log_messages_info = False
+        self._callbacks = []
+        self._connection_status = WalkingPadConnectionStatus.NOT_CONNECTED
+        self._register_controller_callbacks()
+
+    def _register_controller_callbacks(self):
+        self._controller.handler_cur_status = self._on_status_update
+
+    def _begin_cmd(self) -> asyncio.Lock:
+        return asyncio.Lock()
+
+    async def _end_cmd(self):
+        await asyncio.sleep(0.75)
+
+    def _on_status_update(self, sender, data: WalkingPadCurStatus) -> None:
+        """Update current state."""
+
+        status: WalkingPadStatus = {
+            "belt_state": BeltState(data.belt_state),
+            "speed": data.speed / 10,
+            "mode": WalkingPadMode(data.manual_mode),
+            "session_distance": data.dist * 10,
+            "session_running_time": data.time,
+            "session_steps": data.steps,
+            "status_timestamp": data.rtime,
+        }
+
+        if len(self._callbacks) > 0:
+            for callback in self._callbacks:
+                callback(status)
+
+    def register_status_callback(self, callback) -> None:
+        """Register a status callback."""
+        self._callbacks.append(callback)
+
+    @property
+    def mac(self):
+        """Mac address."""
+        return self._ble_device.address
+
+    @property
+    def name(self):
+        """Name."""
+        return self._name
+
+    @property
+    def connection_status(self) -> WalkingPadConnectionStatus:
+        """Connection status."""
+        return self._connection_status
+
+    @property
+    def connected(self) -> bool:
+        """Boolean property to check if the device is connected."""
+        return self._connection_status == WalkingPadConnectionStatus.CONNECTED
+
+    async def connect(self) -> None:
+        """Connect the device."""
+        lock = self._begin_cmd()
+        if self._connection_status == WalkingPadConnectionStatus.CONNECTING:
+            _LOGGER.info("Already connecting to WalkingPad")
+            return
+        _LOGGER.info("Connecting to WalkingPad")
+        async with lock:
+            self._connection_status = WalkingPadConnectionStatus.CONNECTING
+            try:
+                await self._controller.run(self._ble_device)
+                self._connection_status = WalkingPadConnectionStatus.CONNECTED
+            except (BleakError, TimeoutError) as err:
+                _LOGGER.warning("Unable to connect to WalkingPad : %s", err)
+                self._connection_status = WalkingPadConnectionStatus.NOT_CONNECTED
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.warning("Unable to connect to WalkingPad")
+                self._connection_status = WalkingPadConnectionStatus.NOT_CONNECTED
+            await self._end_cmd()
+
+    async def disconnect(self) -> None:
+        """Disconnect the device."""
+        if self._connection_status == WalkingPadConnectionStatus.NOT_CONNECTED:
+            return
+        lock = self._begin_cmd()
+        async with lock:
+            try:
+                await self._controller.disconnect()
+            finally:
+                self._connection_status = WalkingPadConnectionStatus.NOT_CONNECTED
+            await self._end_cmd()
+
+    async def update_state(self) -> None:
+        """Update device state."""
+        # Grab the lock so we don't run while another command is running
+        if self._connection_status == WalkingPadConnectionStatus.NOT_CONNECTED:
+            await self.connect()
+        lock = self._begin_cmd()
+        async with lock:
+            if not self.connected:
+                return
+            try:
+                await self._controller.ask_stats()
+                # Skip callback so we don't reset debouncer
+            except BleakError as err:
+                _LOGGER.warning("Bluetooth error : %s", err)
+                self._connection_status = WalkingPadConnectionStatus.NOT_CONNECTED
